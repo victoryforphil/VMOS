5b06fa65fc6212966aad7e6b3f5c98863d87eb48
alex <VictoryForPhil@gmail.com>
2019-05-07 21:34:16 -0700

    PCA9685 Now Uses Enum Registers Instead of hardcoded ones

--- hardware/control/PCA9685.cpp
+++ hardware/control/PCA9685.cpp
@@ -9,13 +9,13 @@ void PCA9685::init(int freq){
     int prescale = (25000000 / 4096) / freq;
     prescale -= 1.0;
 
-    i2cClient.writeData(0x00, 0x10);
+    i2cClient.writeData(MODE1, 0x10);
     usleep(5000);
-    i2cClient.writeData(0xFE, prescale);
+    i2cClient.writeData(PRE_SCALE, prescale);
     usleep(5000);
-    i2cClient.writeData(0x00, 0x01);
+    i2cClient.writeData(MODE1, 0x01);
     usleep(5000);
-    i2cClient.writeData(0x00, 0x08);
+    i2cClient.writeData(MODE1, 0x08);
 
 
 }
@@ -24,6 +24,6 @@ void PCA9685::setPWM(int chan, int value){
     int low = value & 0xFF;
     int high = value >> 8;
 
-    i2cClient.writeData(0x08 + (chan * 4), low);
-    i2cClient.writeData(0x09 + (chan * 4), high);
+    i2cClient.writeData(LED_BASE + (chan * 4), low);
+    i2cClient.writeData(LED_BASE + 1 + (chan * 4), high);
 }
